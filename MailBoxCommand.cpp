0288880851541389abeed62abbd3265b04d86291
klimentk6 <133498776+Klimentk6@users.noreply.github.com>
2025-05-25 12:40:56 +0300

    Delete MailBoxCommand.cpp

--- MailBoxCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "MailBoxCommand.h"
-
-MailBoxCommand::MailBoxCommand(const char* buffer, Context& ctx)
-	: buffer(buffer), ctx(ctx) {}
-
-void MailBoxCommand::execute()
-{
-	ctx.container.getUserPointer(ctx.loggedId)->printInbox();
-}
